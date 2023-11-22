82993277730396e72ef15fbb004f9afedae8fc97
Jonathan Lao <jon.lao@gmail.com>
2023-11-22 15:37:30 -0600

    added client.js

---
+++ client.js
@@ -0,0 +1,27 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243", // IP address here,
+    port: "50541", // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+  });
+
+  conn.on('data', (data) => {
+      console.log(data);
+      conn.end();
+  });
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = {connect};
